25ec90b1af1f38b15b3d54dd0010c239f1f5bb3b
Raosan Fikri Lillahi <raosanf.lillahi@gmail.com>
2015-10-28 23:02:44 +0700

    pakai angular js buat projects

---
+++ js/controllers/MainController.js
@@ -0,0 +1,80 @@
+app.controller('MainController',['$scope', function($scope){
+ 
+  $scope.projects = 
+    [ 
+      // { 
+      // 	code: '1',
+      //   name: 'SIREX', 
+      //   category: 'Database Management and Scheduling System', 
+      //   img: '531' 
+      // }, 
+      { 
+      	code: '3',
+        name: 'CIEL SBM ITB', 
+        category: 'CMS of the company profile website', 
+        img: '533' 
+      }, 
+      { 
+      	code: '2',
+        name: 'Tresco', 
+        category: 'A CMS for real estate information with custom design wesbitse', 
+        img: '532' 
+      }, 
+      { 
+      	code: '4',
+        name: 'ICDC Indonesia', 
+        category: 'A website for community development', 
+        img: '534' 
+      },
+      // { 
+      // 	code: '5',
+      //   name: 'BWS Papua Barat', 
+      //   category: 'Web for Papua Ministry of Public Works', 
+      //   img: '534' 
+      // }, 
+      { 
+      	code: '6',
+        name: 'Online Kuisioner', 
+        category: 'Online mitigation risk questionnaire', 
+        img: '531' 
+      }, 
+      // { 
+      // 	code: '7',
+      //   name: 'Cryotech', 
+      //   category: 'Crytotech Company Profile Webite', 
+      //   img: '534' 
+      // },  
+      { 
+      	code: '8',
+        name: 'Socentra Institute', 
+        category: 'A Website for Information about Social Entrepreneur', 
+        img: '535' 
+      }, 
+      // { 
+      // 	code: '9',
+      //   name: 'SKHA Consulting Company Profile', 
+      //   category: 'CMS company profile with custom design', 
+      //   img: '625' 
+      // }, 
+      // { 
+      // 	code: '10',
+      //   name: 'spiro+', 
+      //   category: 'Model of integrated hydroponics farm with automatic control system', 
+      //   img: '534' 
+      // },  
+      // { 
+      // 	code: '11',
+      //   name: 'Inagro', 
+      //   category: 'One stop marketplace for agroindustrial products', 
+      //   img: '534' 
+      // },  
+      { 
+      	code: '12',
+        name: 'SIPENCATAR', 
+        category: 'Online registration and integrated Computer Based Test for selection of Calon Taruna by BPSDM Kemenhub', 
+        img: '625' 
+      },  
+    
+    ]
+	
+}]);
